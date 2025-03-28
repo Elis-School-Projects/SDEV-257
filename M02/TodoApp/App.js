f20d8e782f500b7f897f03e4933075c9749b8f64
Eli Walters <ewalters37@ivytech.edu>
2025-03-28 13:11:18 -0400

    Add Module 2 Assignments

---
+++ M02/TodoApp/App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Task from './components/Task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      {/* Todays Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* !! List of Tasks */}
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
+          <Task text={"Task 3"} />
+          <Task text={"Task 4"} />
+          <Task text={"Task 5"} />
+        </View>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+});
