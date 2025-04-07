2329e635749f79356ba6c3dfd3d44b7c88a24c9b
Eli Walters <eliwalters@Elis-MacBook-Pro.local>
2025-04-07 17:41:39 -0400

    Add M04 Geo-Location Assignment

--- M04/Geo-Location/App.js
+++ M04/Geo-Location/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from "react-native-maps";
+import * as Location from 'expo-location';
+import styles from "./styles";
 
 export default function App() {
+  Location.requestForegroundPermissionsAsync();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.mapView} showsUserLocation={true} followsUserLocation={true} showsPointsOfInterest={false}>
+        <Marker 
+          title="The Cheesecake Factory"
+          description="Local eatery with excellent desert options!"
+          coordinate={{
+            latitude:39.633217081392196,
+            longitude: -86.1231590864407
+          }}
+        />
+      </MapView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
